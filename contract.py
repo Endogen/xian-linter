24de7e5dd04286113c4dae2183c1fe4221268f23
Endogen <Endogen@users.noreply.github.com>
2025-01-14 03:24:58 +0100

    Added example contract with some errors

--- contract.py
+++ contract.py
@@ -1,17 +1,14 @@
-counter = Variable()
-messages = Hash()
+balances = Hash(default_value=0)
+metadata = Hash()
 
-@construct
-def init():
-    counter.set(0)
+def transfer_from(amount: float, to: str, main_account: str):
+    assert amount > 0, 'Cannot send negative balances!'
+    assert balances[main_account, ctx.caller] >= amount, \
+    f'Not enough coins approved to send! You have {balances[main_account, ctx.caller]} and are trying to spend {amount}'
 
-@export
-def save_msg(msg: str, recipient: str):
-    counter.set(counter.get() + 1)
+    dir
 
-    messages[counter.get()] = {
-        'sender': ctx.signer,
-        'message': msg,
-        'receiver': recipient,
-        'timestamp': now
-    }
+    assert balances[main_account] >= amount, 'Not enough coins to send!'
+    balances[main_account, ctx.caller] -= amount
+    balances[main_account] -= amount
+    balances[to] += amount
